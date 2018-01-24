bbe3e224d94428bd818a16d4689ff9c01a9d88d1
MinnDevelopment <business@minnced.club>
2018-01-24 16:37:51 +0100

    Updated scripts

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-#!/sh
+#!/bin/bash
 accept="application/vnd.github.v3+json"
 base_url="https://api.github.com"
 
@@ -6,19 +6,34 @@ owner="discordapp"
 repo="discord-rpc"
 
 curl -H "Accept: $accept" \
-     -H "Authorization: $GIT_OAUTH_TOKEN" \
     "$base_url/repos/$owner/$repo/releases/latest" > latest.json
 
-./gradlew setup
+./gradlew clean setup
 resources="src/main/resources"
+echo moving binaries...
 mv "win/discord-rpc/win64-dynamic/lib/discord-rpc.lib" "$resources/win32-x86-64/discord-rpc.dll"
 mv "win/discord-rpc/win32-dynamic/lib/discord-rpc.lib" "$resources/win32-x86/discord-rpc.dll"
 mv "linux/discord-rpc/linux-dynamic/lib/libdiscord-rpc.so" "$resources/linux-x86-64/libdiscord-rpc.so"
 mv "osx/discord-rpc/osx-dynamic/lib/libdiscord-rpc.dylib" "$resources/darwin/libdiscord-rpc.dylib"
 
-rm latest.json
-
+version=$(<tag_name.txt)
+rm latest.json tag_name.txt
+echo starting compilation...
 ./gradlew build
 
 rm -rf linux win osx
 
+git add *
+git status
+
+echo
+echo "Current tags:"
+git tag
+echo
+echo "To be added: $version"
+
+read -n1 -p "Press any key to continue..."
+
+git commit -m "Updated to release $version"
+git tag -a $version -m $version
+git push --tags origin master
